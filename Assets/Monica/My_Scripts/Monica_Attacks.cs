218129d360f9d428174c1bc49f67d462a685e3b1
MoniMarroquin <monicamarroquin136@yahoo.com>
2024-12-08 23:57:18 -0600

    end of day 1

---
+++ Assets/Monica/My_Scripts/Monica_Attacks.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Attacking_Monica
+{
+    public class Monica_DontRemember : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        // if health goes below 30 will run to nearest healing
+        // if health is between 100-70 will stand still and launch ranged attacks?
+        // if health is beiween 69 and 35 
+    }
+}
---
+++ Assets/Monica/My_Scripts/Monica_Healing.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monica_Healing
+{
+    public class Monica_Healing : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        // if enemey is here helth goes up
+        // crystals haveing a healing boost/ make the players attack weeker 
+    }
+}
