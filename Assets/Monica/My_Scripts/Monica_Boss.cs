8fc6790239ede5d16653ae9a096e50b3972a3c68
MoniMarroquin <monicamarroquin136@yahoo.com>
2024-12-08 22:50:10 -0600

    Added crystals

--- Assets/Monica/My_Scripts/Monica_Boss.cs
+++ Assets/Monica/My_Scripts/Monica_Boss.cs
@@ -17,5 +17,13 @@ namespace MonicaBoss
         {
 
         }
+        // scripts needed damage, ai so this one depending on what the player does
+
+        // healing cristals that give the boss more health better to have them affect the player so when they are distroyed the players attack gets higher 
+        // minions depleat the boss so the more there or the weeker the boss, amount decreses each level?
+        // knock the boss out of a place where if they stay they get healed?
+        // boss trying to make it flashey/ its an act so not a real fight to the death
+        // object that the boss gets near and can rapidly gain health 
+        // the room is a long halway and the boss runs away, the way is covered in traps 
     }
 }
